bd1a97d94a064d1c73953735470225189c71e8fc
Anthony <achen3741@gmail.com>
2021-03-30 17:28:59 -0400

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Anthony Chen on 2021-03-30.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
